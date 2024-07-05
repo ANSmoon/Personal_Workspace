f1b62dc24ced9afdbfd755136a9480f407293cbb
ANSMOON <munbeumsu@gmail.com>
2024-07-05 15:14:49 +0900

    Shortest Path Code

--- newTest/src/shortestPath/Main.java
+++ newTest/src/shortestPath/Main.java
@@ -1,12 +1,13 @@
 /*
  * Author : 문범수
  * Date : 2024-07-04
- * subject :
- * page :
- * main :
- * issue :
+ * subject : 최단경로 구하기
+ * page : https://www.acmicpc.net/problem/1753
+ * main : Dijkstra
+ * issue : Memory 초과, 완탐방식 불가
+ * solution : Adjacency List, Priority Queue
  * name : Main.java
- * duration : h
+ * duration : 6h
  * no : 1753
  */
 
@@ -15,12 +16,12 @@ package shortestPath;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int V, E, K, dep, dest, weight;
-    static int[] ans;
-    static int[][] graph;
+    static int V, E, K; // V : vertex / E : edge / K : 시작 vertex
+    static int[] dist; // 각 정점까지 최소 거리
+    static List<List<int[]>> graph; // list 내에 list 형식으로 담는다.
     static StringTokenizer token;
 
     public static void main(String[] args) throws IOException {
@@ -29,51 +30,74 @@ public class Main {
         V = Integer.parseInt(token.nextToken());
         E = Integer.parseInt(token.nextToken());
         K = Integer.parseInt(reader.readLine());
-        dep = 0;
-        dest = 0;
-        weight = 0;
-        graph = new int[V + 1][V + 1];
-        ans = new int[V + 1];
 
-        // 정점간의 가중치의 정보를 담은 graph를 2차원 배열에 저장
-        for(int i = 1; i <= E; i++){
+
+        graph = new ArrayList<>();
+
+        // 내부 list 초기화 부분
+        for (int i = 0; i <= V; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[K] = 0;
+
+        // Adjacency List Create
+        // 각 정점별로 연결된 정점과 가중치를 저장하는 list
+        for (int i = 0; i < E; i++) {
             token = new StringTokenizer(reader.readLine());
-            dep = Integer.parseInt(token.nextToken());
-            dest = Integer.parseInt(token.nextToken());
-            weight = Integer.parseInt(token.nextToken());
+            int dep = Integer.parseInt(token.nextToken());
+            int des = Integer.parseInt(token.nextToken());
+            int weight = Integer.parseInt(token.nextToken());
 
-            // 저장된 가중치가 0일 경우 그대로 삽입
-            if(graph[dep][dest] == 0){
-                graph[dep][dest] = weight;
-            }
-
-            // 저장된 가중치가 있을경우 비교해서 작은 값을 삽입
-            else{
-                graph[dep][dest] = Math.min(weight, graph[dep][dest]);
-            }
+            // 시작점을 u로 갖는 모든 연결 점의 정보가 list 형식으로 저장되고 있임
+            graph.get(dep).add(new int[]{des, weight});
         }
 
         // 입력 test
-        for(int i = 0; i < V; i++){
-            for(int j = 0; j < V; j++){
+        /*for(int i = 0; i <= V; i++){
+            for(int j = 0; j <= V; j++){
                 System.out.print(graph[i][j] + " ");
             }
             System.out.println();
-        }
+        }*/
 
-        for(int i = 1; i <= V; i++){
-            for(int j = 1; j <= V; j++){
-                // 1과 연결된 간선의 가중치 저장
-                if(i == 1 && graph[i][j] != 0) {
-                    ans[j] = graph[i][j];
-                }
+        dijkstra(K);
+
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                System.out.println("INF");
+            } else {
+                System.out.println(dist[i]);
             }
         }
+    }
 
+    static void dijkstra(int start) {
+        // 우선순위 Queue 사용
+        // int[] 배열에서 두번째 요소를 기준으로 정렬함. 결과적으로 거리가 가장 짧은 경로만을 가져올 수 있음
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(a -> a[1]));
+        pq.add(new int[]{start, 0});
 
+        while (!pq.isEmpty()) {
+            int[] tmp = pq.poll();
+            int dep = tmp[0]; // start
+            int curDist = tmp[1]; // distance
 
-        for(int i = 1; i <= V; i++){
-            System.out.print(ans[i] + " ");
+            // 해당 정점까지의 거리가 기존의 거리보다 크면 넘어간다.
+            if (curDist > dist[dep]) continue;
+
+            for (int[] neighbor : graph.get(dep)) {
+                int des = neighbor[0]; // 도착점
+                int weight = neighbor[1]; // 가중치
+                int newDist = curDist + weight;
+
+                if (newDist < dist[des]) {
+                    dist[des] = newDist;
+                    pq.add(new int[]{des, newDist});
+                }
+            }
         }
     }
 }
